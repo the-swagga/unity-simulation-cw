f3a83fd52c2131255139cd5db0a50378af9ecf7b
the-swagga <144693488+the-swagga@users.noreply.github.com>
2025-10-11 19:50:06 +0100

    Implement water generation for tiles

--- Assets/Scripts/Map/GrasslandsTile.cs
+++ Assets/Scripts/Map/GrasslandsTile.cs
@@ -8,6 +8,10 @@ public class GrasslandsTile : BaseTile
     [SerializeField] private float treeDensity = 0.05f;
     [SerializeField] private Vector3 treeScale = Vector3.one;
     [SerializeField] private float waterProb = 0.1f;
+    [SerializeField] private Vector3 waterScale = Vector3.one;
+    [SerializeField] private Vector2 waterScaleRange = new Vector2(0.1f, 1.0f);
+    [SerializeField] private float waterYPos = 0.0f;
+    [SerializeField] private GameObject waterPrefab;
     [SerializeField] private GameObject[] treePrefabs;
     [SerializeField] private GameObject[] enemyPrefabs;
     [SerializeField] private GameObject[] itemPrefabs;
@@ -42,12 +46,27 @@ public class GrasslandsTile : BaseTile
             Quaternion treeRot = Quaternion.Euler(0, Random.Range(0, 360), 0);
 
             GameObject tree = Instantiate(treePrefab, treePos, treeRot, transform);
-            tree.transform.localScale = treeScale;
+            Vector3 tileScale = transform.localScale;
+            tree.transform.localScale = new Vector3(treeScale.x / tileScale.x, treeScale.y / tileScale.y, treeScale.z / tileScale.z);
         }
     }
 
     private void GenerateWater()
     {
+        if (waterPrefab == null) return;
 
+        Vector2 tileSize = GetTileSize();
+        float randomNum = Random.Range(0.0f, 1.0f);
+        if (randomNum <= waterProb)
+        {
+            float randomX = Random.Range(-tileSize.x / 2.0f, tileSize.x / 2.0f);
+            float randomZ = Random.Range(-tileSize.y / 2.0f, tileSize.y / 2.0f);
+            Vector3 waterPos = transform.position + new Vector3(randomX, waterYPos, randomZ);
+
+            GameObject water = Instantiate(waterPrefab, waterPos, Quaternion.identity, transform);
+            Vector3 tileScale = transform.localScale;
+            float randomScale = Random.Range(waterScaleRange.x, waterScaleRange.y);
+            water.transform.localScale = new Vector3((waterScale.x * randomScale) / tileScale.x, waterScale.y / tileScale.y, (waterScale.z * randomScale) / tileScale.z);
+        }
     }
 }
